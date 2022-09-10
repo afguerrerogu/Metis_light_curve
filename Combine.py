b305f4c894aa5ae9b978690a57ba43bb560ae736
Andres Felipe Guerrero Guio <54873780+afguerrerogu@users.noreply.github.com>
2022-09-10 14:10:24 -0500

    Update Combine.py

--- Combine.py
+++ Combine.py
@@ -10,3 +10,25 @@ import numpy as np
 
 from convenience_functions import show_image
 
+# Use custom style for larger fonts and figures
+plt.style.use('guide.mplstyle')
+
+def Combine(path):
+  
+  calibrated_path = Path(path)
+  reduced_images = ccdp.ImageFileCollection(calibrated_path)
+  
+  calibrated_biases = reduced_images.files_filtered(imagetyp='bias', include_path=True)
+
+  combined_bias = ccdp.combine(calibrated_biases,
+                             method='average',
+                             sigma_clip=True, sigma_clip_low_thresh=5, sigma_clip_high_thresh=5,
+                             sigma_clip_func=np.ma.median, sigma_clip_dev_func=mad_std,
+                             mem_limit=350e6
+                            )
+
+  combined_bias.meta['combined'] = True
+
+  combined_bias.write(calibrated_path / 'combined_bias.fit')
+  
+  Return None
